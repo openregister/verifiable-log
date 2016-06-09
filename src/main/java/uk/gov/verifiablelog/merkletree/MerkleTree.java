e46162c428c364473855b1eff7b4bca366928ea0
michaelabroschomb <michaela.broschomb@digital.cabinet-office.gov.uk>
2016-06-09 12:43:07 +0100

    Refactor to use consistent parameter names

--- src/main/java/uk/gov/verifiablelog/merkletree/MerkleTree.java
+++ src/main/java/uk/gov/verifiablelog/merkletree/MerkleTree.java
@@ -20,11 +20,11 @@ public class MerkleTree {
     }
 
     public byte[] currentRoot() {
-        return subtreeHash(0, leafSizeDAOFunction.get(), leafDAOFunction.apply(0, leafSizeDAOFunction.get()));
+        return subtreeHash(leafSizeDAOFunction.get(), leafDAOFunction.apply(0, leafSizeDAOFunction.get()));
     }
 
     public List<byte[]> pathToRootAtSnapshot(int leafIndex, int snapshotSize) {
-        return subtreePathAtSnapshot(leafIndex, 0, snapshotSize);
+        return subtreePathAtSnapshot(leafIndex, snapshotSize, leafDAOFunction);
     }
 
     public List<byte[]> snapshotConsistency(int snapshot1, int snapshot2) {
@@ -32,62 +32,62 @@ public class MerkleTree {
             // RFC 6962 §2.1.2 assumes `0 < m < n`; we assume `0 < m <= n`
             throw new IllegalArgumentException("snapshot1 must be strictly positive");
         }
-        return subtreeSnapshotConsistency(0, snapshot2, snapshot1);
+        return subtreeSnapshotConsistency(snapshot1, snapshot2, true, leafDAOFunction);
     }
 
-    private List<byte[]> subtreeSnapshotConsistency(int start, int end, int m) {
-        int size = end - start;
-        if (m == size) {
-            if (start == 0) {
+    private List<byte[]> subtreeSnapshotConsistency(int low, int high, boolean startFromOldRoot, BiFunction<Integer, Integer, Iterator<byte[]>> subtreeDAOFunction) {
+        if (low == high) {
+            if (startFromOldRoot) {
                 // this is the b == true case in RFC 6962
                 return new ArrayList<>();
             }
             List<byte[]> consistencySet = new ArrayList<>();
-            consistencySet.add(subtreeHash(start, end, leafDAOFunction.apply(start, end)));
+            consistencySet.add(subtreeHash(high, subtreeDAOFunction.apply(0, high)));
             return consistencySet;
         }
-        int k = Util.k(size);
-        int mid = start + k;
-        if (m <= k) {
-            List<byte[]> subtreeConsistencySet = subtreeSnapshotConsistency(start, mid, m);
-            subtreeConsistencySet.add(subtreeHash(mid, end, leafDAOFunction.apply(mid, end)));
+        int k = Util.k(high);
+        if (low <= k) {
+            List<byte[]> subtreeConsistencySet = subtreeSnapshotConsistency(low, k, startFromOldRoot, subtreeDAOFunction);
+            subtreeConsistencySet.add(subtreeHash(high - k, subtreeDAOFunction.apply(k, high)));
             return subtreeConsistencySet;
         } else {
-            List<byte[]> subtreeConsistencySet = subtreeSnapshotConsistency(mid, end, m - k);
-            subtreeConsistencySet.add(subtreeHash(start, mid, leafDAOFunction.apply(start, mid)));
+            List<byte[]> subtreeConsistencySet = subtreeSnapshotConsistency(low - k, high - k, false, shiftBaseIndex(subtreeDAOFunction, k));
+            subtreeConsistencySet.add(subtreeHash(k, subtreeDAOFunction.apply(0, k)));
             return subtreeConsistencySet;
         }
+    }
+
+    private BiFunction<Integer, Integer, Iterator<byte[]>> shiftBaseIndex(BiFunction<Integer, Integer, Iterator<byte[]>> subtreeDAOFunction, int k) {
+        return (l, h) -> subtreeDAOFunction.apply(l + k, h + k);
     }
 
     // audit path within subtree of leaves from start (inclusive) to end (exclusive)
-    private List<byte[]> subtreePathAtSnapshot(int leafIndex, int start, int end) {
-        int size = end - start;
-        if (size <= 1) {
+    private List<byte[]> subtreePathAtSnapshot(int leafIndex, int snapshotSize, BiFunction<Integer, Integer, Iterator<byte[]>> subtreeDAOFunction) {
+        if (snapshotSize <= 1) {
             return new ArrayList<>();
         }
-        int mid = start + Util.k(size);
-        if (leafIndex < mid) {
-            List<byte[]> subtreePath = subtreePathAtSnapshot(leafIndex, start, mid);
-            subtreePath.add(subtreeHash(mid, end, leafDAOFunction.apply(mid, end)));
+        int k = Util.k(snapshotSize);
+        if (leafIndex < k) {
+            List<byte[]> subtreePath = subtreePathAtSnapshot(leafIndex, k, subtreeDAOFunction);
+            subtreePath.add(subtreeHash(snapshotSize - k, subtreeDAOFunction.apply(k, snapshotSize)));
             return subtreePath;
         } else {
-            List<byte[]> subtreePath = subtreePathAtSnapshot(leafIndex, mid, end);
-            subtreePath.add(subtreeHash(start, mid, leafDAOFunction.apply(start, mid)));
+            List<byte[]> subtreePath = subtreePathAtSnapshot(leafIndex - k, snapshotSize - k, shiftBaseIndex(subtreeDAOFunction, k));
+            subtreePath.add(subtreeHash(k, subtreeDAOFunction.apply(0, k)));
             return subtreePath;
         }
     }
 
-    // hash of subtree from start (inclusive) to end (exclusive)
-    private byte[] subtreeHash(int start, int end, Iterator<byte[]> iterator) {
-        int size = end - start;
+    // hash of subtree of given size
+    private byte[] subtreeHash(int size, Iterator<byte[]> iterator) {
         if (size == 0) {
             return emptyTree();
         } else if (size == 1) {
             return Util.leafHash(iterator.next(), messageDigest);
         } else {
-            int mid = start + Util.k(size);
-            byte[] leftSubtreeHash = subtreeHash(start, mid, iterator);
-            byte[] rightSubtreeHash = subtreeHash(mid, end, iterator);
+            int k = Util.k(size);
+            byte[] leftSubtreeHash = subtreeHash(k, iterator);
+            byte[] rightSubtreeHash = subtreeHash(size - k, iterator);
             return Util.branchHash(leftSubtreeHash, rightSubtreeHash, messageDigest);
         }
     }
