a112be00ddaaf775808250aece8ff18478831c9b
Chris Duraj <christopherduraj@gmail.com>
2016-06-22 14:27:12 +0100

    Remove unused utils and legacy dto test item class

--- src/test/java/uk/gov/verifiablelog/merkletree/TestUtil.java
+++ src/test/java/uk/gov/verifiablelog/merkletree/TestUtil.java
@@ -1,20 +1,9 @@
 package uk.gov.verifiablelog.merkletree;
 
 import javax.xml.bind.DatatypeConverter;
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
-
-class MerkleTreeTestUnit {
-    public MerkleTree merkleTree;
-    public List<byte[]> leaves;
-
-    public MerkleTreeTestUnit(MerkleTree merkleTree, List<byte[]> leaves) {
-        this.merkleTree = merkleTree;
-        this.leaves = leaves;
-    }
-}
 
 public class TestUtil {
     public static MerkleTree makeMerkleTree(List<byte[]> entries) {
@@ -23,11 +12,6 @@ public class TestUtil {
 
     public static MerkleTree makeMerkleTree(List<byte[]> entries, MemoizationStore memoizationStore) {
         return new MerkleTree(Util.sha256Instance(), entries::get, entries::size, memoizationStore);
-    }
-
-    public static MerkleTreeTestUnit makeMerkleTreeTestUnit(MemoizationStore memoizationStore) {
-        List<byte[]> leafValues = new ArrayList<>();
-        return new MerkleTreeTestUnit(makeMerkleTree(leafValues, memoizationStore), leafValues);
     }
 
     public static List<String> bytesToString(List<byte[]> listOfByteArrays) {
